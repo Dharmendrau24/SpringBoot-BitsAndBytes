380b6e57d0e09ee678fb2a9d9bf0908266961bb4
dharmendra <dharmendraupadhyay24@gmail.com>
2025-04-26 15:33:55 +0530

    category created

---
+++ product/src/main/java/com/bitsnbytes/product/service/Productservice.java
@@ -0,0 +1,13 @@
+package com.bitsnbytes.product.service;
+
+import com.bitsnbytes.product.dto.ProductDTO;
+import com.bitsnbytes.product.entity.Product;
+import com.bitsnbytes.product.mapper.ProductMapper;
+import com.bitsnbytes.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Productservice {
+    private ProductRepository productRepository;
+
+}
